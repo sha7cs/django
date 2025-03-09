10b01ebfe51ad59521616f2768930cf4d5b8b3ae
shatha <sha7cs@gmail.com>
2025-03-09 12:55:54 +0300

    lab 6

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('html5/text/formatting', views.formatting, name="books.formatting"),
     path('html5/listing', views.listing, name="books.listing"),
     path('html5/tables', views.tables, name="books.tabls"),
+    path('search/', views.search, name= "books.search"),
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -61,3 +61,31 @@ def listing(request):
 
 def tables(request):
     return render(request, 'bookmodule/tables.html' )
+
+def search(request):
+    if request.method == "GET":
+        return render(request, 'bookmodule/listbbooks.html')
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        # now filter
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower():contained = True
+            
+            if contained: newBooks.append(item)
+        return render(request, 'bookmodule/listbbooks.html', {'books':newBooks})
+ 
+
+    # return render(request, 'bookmodule/listbbooks.html' )
+
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
+ 
